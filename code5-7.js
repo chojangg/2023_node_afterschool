5725b12d93c8124c76bdc78fc39cbc05ea1018bd
chojangg <s2115@e-mirim.hs.kr>
2023-03-23 19:55:12 +0900

    2023.03.23

---
+++ code5-7.js
@@ -0,0 +1,13 @@
+// 변수를 선언
+let inputA = "52";
+let inputB = "52.273";
+let inputC = "1401동";
+
+// parseInt() 함수의 기본적인 사용
+console.log(parseInt(inputA))
+
+// parseInt() 함수의 parseFloat() 함수의 차이
+console.log(parseInt(inputB));
+console.log(parseFloat(inputB));
+
+// 문자열로
